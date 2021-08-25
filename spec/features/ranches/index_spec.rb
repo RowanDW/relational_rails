fb46793d498cd81d7ff89e1aaf20506f77ee2bb9
Michael Mattson <michael.p.mattson@gmail.com>
2021-08-25 15:57:13 -0600

    Add attributes to ranches index

--- spec/features/ranches/index_spec.rb
+++ spec/features/ranches/index_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Ranches index' do
-  it 'displays the name of each ranch' do
+  before(:each) do
     @ranch_1 = Ranch.create!(name: "Fernando's Fine Bovines", max_capacity: 50, certified_humane: true)
     @ranch_2 = Ranch.create!(name: "Janie's Jolly Cow Corral", max_capacity: 80, certified_humane: false)
+  end
 
+  it 'displays the name of each ranch' do
     visit "/ranches"
 
-    expect(page).to have_content(@ranch_1.name)
-    expect(page).to have_content(@ranch_2.name)
+    expect(page).to have_content("Fernando's Fine Bovines")
+    expect(page).to have_content("Janie's Jolly Cow Corral")
+
+    expect(page).to have_content("Max Capacity: 50")
+    expect(page).to have_content("Max Capacity: 80")
+
+    expect(page).to have_content("Certified Humane: true")
+    expect(page).to have_content("Certified Humane: false")
   end
 end
