482c54b2f626efe735e88ebf885704ffe68169ac
Michael Mattson <michael.p.mattson@gmail.com>
2021-08-25 16:19:44 -0600

    Add ranches show page

--- app/controllers/ranches_controller.rb
+++ app/controllers/ranches_controller.rb
@@ -2,4 +2,8 @@ class RanchesController < ApplicationController
   def index
     @ranches = Ranch.all
   end
+
+  def show
+    @ranch = Ranch.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
 
   get '/', to: 'welcome#index'
   get '/farms', to: 'farms#index'
-  get '/ranches', to: 'ranches#index'
   get '/farms/:id', to: 'farms#show'
-  get '/crops', to: 'crops#index'
-  get '/crops/:id', to: 'crops#show'
   get '/farms/:id/crops', to: 'farm_crops#index'
 
+  get '/crops', to: 'crops#index'
+  get '/crops/:id', to: 'crops#show'
+
+  get '/ranches', to: 'ranches#index'
+  get '/ranches/:id', to: 'ranches#show'
 end
