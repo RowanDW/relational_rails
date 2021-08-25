b0ad1d2541ffe1b1e7d32c8884aab9ba12dc31a3
Michael Mattson <michael.p.mattson@gmail.com>
2021-08-25 16:10:39 -0600

    Remove attributes

--- spec/features/ranches/index_spec.rb
+++ spec/features/ranches/index_spec.rb
@@ -1,21 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Ranches index' do
-  before(:each) do
-    @ranch_1 = Ranch.create!(name: "Fernando's Fine Bovines", max_capacity: 50, certified_humane: true)
-    @ranch_2 = Ranch.create!(name: "Janie's Jolly Cow Corral", max_capacity: 80, certified_humane: false)
-  end
-
   it 'displays the name of each ranch' do
+    ranch_1 = Ranch.create!(name: "Fernando's Fine Bovines", max_capacity: 50, certified_humane: true)
+    ranch_2 = Ranch.create!(name: "Janie's Jolly Cow Corral", max_capacity: 80, certified_humane: false)
+
     visit "/ranches"
 
     expect(page).to have_content("Fernando's Fine Bovines")
     expect(page).to have_content("Janie's Jolly Cow Corral")
-
-    expect(page).to have_content("Max Capacity: 50")
-    expect(page).to have_content("Max Capacity: 80")
-
-    expect(page).to have_content("Certified Humane: true")
-    expect(page).to have_content("Certified Humane: false")
   end
 end
