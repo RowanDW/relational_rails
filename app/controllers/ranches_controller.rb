b0382c05dc2ed6fec68cfd2f598e5901243be661
Michael Mattson <michael.p.mattson@gmail.com>
2021-08-27 19:22:24 -0600

    Add strong params private method, move pamars out of create

--- app/controllers/ranches_controller.rb
+++ app/controllers/ranches_controller.rb
@@ -11,14 +11,12 @@ class RanchesController < ApplicationController
   end
 
   def create
-    Ranch.create(name: params[:name], max_capacity: params[:max_capacity], certified_humane: params[:certified_humane])
+    Ranch.create(ranch_params)
     redirect_to '/ranches'
   end
 
-  # private
-  # def ranch_params
-  #   params.permit(:name)
-  #   params.permit(:max_capacity)
-  #   params.permit(:certified_humane)
-  # end
+  private
+  def ranch_params
+    params.permit(:name, :max_capacity, :certified_humane)
+  end
 end
